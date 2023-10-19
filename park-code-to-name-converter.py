d53111004bc7ebde5697e82a2c5a0909e7507120
j-nicks <140603506+j-nicks@users.noreply.github.com>
2023-10-19 18:01:39 +0100

    add park code dictonary (v0.1)

--- park-code-to-name-converter.py
+++ park-code-to-name-converter.py
@@ -1,1 +1,44 @@
-# Park Code to Name converter
+### Park Code to Name converter
+# Dictionary for park codes
+park_codes = {
+    "AH": "Allhallows",             #changed name
+    "BD": "Blue Dolphin",
+    "BE": "Berwick",
+    "BR": "Burnham On Sea",
+    "CC": "Caister-on-Sea",
+    "CF": "Church Farm",
+    "CG": "Cala Gran",
+    "CH": "Combe Haven",
+    "CT": "Craig Tara",
+    "DE": "Devon Cliffs",
+    "DF": "Doniford Bay",
+    "FG": "Far Grange",
+    "GR": "Greenacres",
+    "GS": "Golden Sands",
+    "HA": "Haggerston Castle",
+    "HM": "Hafan Y Mor",
+    "HO": "Hopton",
+    "KP": "Kiln Park",
+    "LA": "Lakeland",
+    "LS": "Littlesea",
+    "LY": "Lydstep Beach",
+    "MM": "Marton Mere",
+    "OR": "The Orchards",
+    "P*": "***",                    #removed until confirmed ok
+    "PH": "Presthaven",
+    "PS": "Perran Sands",
+    "PV": "Primrose Valley",
+    "QW": "Quay West",
+    "RE": "Reighton Sands",
+    "R*": "***",                    #removed until confirmed ok
+    "RP": "Rockley Park",
+    "RV": "Riviere Sands",
+    "SA": "Seashore",
+    "SE": "Seton Sands",
+    "SN": "Skegness Holiday Park",
+    "SV": "Seaview",
+    "TP": "Thorpe Park",            #changed name  
+    "TW": "Thornwick",
+    "WD": "Wild Duck",
+    "WM": "Weymouth Bay"
+}
